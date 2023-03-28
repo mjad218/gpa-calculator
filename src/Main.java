f852a82b55b03e755d873d5f7f97566d00b86ff3
Mohamed Gad <59421219+mjad218@users.noreply.github.com>
2023-03-28 21:40:30 +0200

    project structure - Parser and Calc classes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+import java.io.*;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String myfile, myline;
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter File Name: ");
+        myfile = scan.nextLine();
+
+        try
+        {
+            FileReader fileReader = new FileReader(myfile);
+            BufferedReader bufread = new BufferedReader(fileReader);
+
+            // reading the file, line by line
+            while((myline = bufread.readLine()) != null)
+                System.out.println(myline);
+
+            bufread.close();
+        }
+        catch(IOException e)
+        {
+            System.out.println("Exception: " +e);
+        }
+
     }
 }
